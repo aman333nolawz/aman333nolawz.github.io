592702bb7ca7f515ac51f5623bef98960d8cdf1d
B34RN00B <b34r@blitzhack.xyz>
2025-07-09 20:21:17 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://D0s4.github.io',
+  site: 'https://b34rn008.github.io',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
